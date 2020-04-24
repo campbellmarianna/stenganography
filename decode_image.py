9ee942ac6ef9ed46ea000b9054a5fcd49bf53023
Marianna <mckd2018@gmail.com>
2020-04-23 21:56:32 -0400

    [add] memory efficiency

--- decode_image.py
+++ decode_image.py
@@ -14,9 +14,7 @@ def decode_image(file_location):
 
     for x in range(0, x_size):
         for y in range(0, y_size):
-            r = red_channel.getpixel((x, y))
-            b_red = bin(r)
-            LSB = b_red[-1]
+            LSB = bin(red_channel.getpixel((x, y)))[-1]
             if LSB == '1':
                 pixels[x, y] = (0, 0, 0)
             else:
