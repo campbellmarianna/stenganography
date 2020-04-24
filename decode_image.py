612670127a5d321023bf7ca7499adb0c709614bb
Marianna <mckd2018@gmail.com>
2020-04-23 21:06:05 -0400

    [upd] finished code but still not producing output

--- decode_image.py
+++ decode_image.py
@@ -1,8 +1,5 @@
 from PIL import Image 
 def decode_image(file_location):
-    '''
-    Return a very secret message.
-    '''
     # Returns an image object
     encoded_image = Image.open(file_location)
     print("encoded_image:", encoded_image)
@@ -11,34 +8,39 @@ def decode_image(file_location):
     print('red_channel:', red_channel)
 
     x_size = encoded_image.size[0]
-    print('x_size:', type(x_size))
+    # print('x_size:', type(x_size))
     y_size = encoded_image.size[1]
-    print('y_size:', y_size)
+    # print('y_size:', y_size)
 
     decoded_image = Image.new("RGB", encoded_image.size)
     # decoded_image.show()
     pixels = decoded_image.load()
-    print('pixels', type(pixels))
-    #TODO: Fill in decoding functionality
+    # print('pixels', type(pixels))
+    # Fill in decoding functionality
     # iterate through each pixel in the encoded image and set the decode_image pixel to be (0,0,0) or (255,255,255) depending on the value of that LSB
     # Loop over the range of x_size (`x`)
     for x in range(0, x_size):
-        print("***x:", x)
+        # print("***x:", x)
         # Loop over the range of y_size (`y`)
         for y in range(0, y_size):
-            print("***y:", y)
+            # print("***y:", y)
             coordinates = x, y
             # Get RGB values from red_channel via `getpixel(x, y)`
-            r, g, b = encoded_image.getpixel(coordinates)
+            # r = red_channel.getpixel(coordinates)
+            # print("what is red_channel.getpixel(coordinates):",
+            #       red_channel.getpixel(coordinates))
+            r = red_channel.getpixel(coordinates)
             # Convert red value in returned tuple to a binary string via `bin()`
             b_red = str(bin(r))
-            print("b_red", b_red)
+            # print("b_red", b_red)
             # Grab the LSB (right-most value) from the binary string
             LSB = int(b_red[-1])
+            # print("LSB", LSB)
             # Perform a check to see if resulting LSB value is `1`
             #   Write `pixels[x, y]` to to black `(0, 0, 0)`
             if LSB == 1:
-                pixels[x,y] == (0, 0, 0)
+                # print('What does the pixel look like right now:', pixels[x, y])
+                pixels[x,y] == (0, 0 , 0)
             # Otherwise
             #   Write `pixels[x, y]` to white `(255, 255, 255)`
             else:
